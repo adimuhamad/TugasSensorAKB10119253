52bad30cfa04947937f25e58cbe5f0dd4ca5fb37
adimuhamad <adi.10119253@mahasiswa.unikom.ac.id>
2022-07-02 01:13:56 +0700

    add splash activity

---
+++ app/src/main/java/id/mamr/tugassensorakb10119253/OnboardingActivity.java
@@ -0,0 +1,14 @@
+package id.mamr.tugassensorakb10119253;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class OnboardingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_onboarding);
+    }
+}
